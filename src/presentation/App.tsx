4e8349c7887a14e9c816558473e0ed3cdb932fbe
josemorista <josemorista@id.uff.br>
2021-03-28 16:21:04 -0300

    feat: added NewTransactionModal

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,12 +1,25 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Dashboard } from './components/pages/Dashboard';
 import { Header } from './components/shared/Header';
+import Modal from 'react-modal';
+
+Modal.setAppElement('root');
 
 export const App: FC = () => {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
 	return (
 		<div className="App">
-			<Header />
+			<Header onNewTransactionButtonClick={() => {
+				setIsNewTransactionModalOpen(true);
+			}} />
 			<Dashboard />
+			<Modal isOpen={isNewTransactionModalOpen}
+				onRequestClose={() => {
+					setIsNewTransactionModalOpen(false);
+				}}>
+				<h1>Cadastrar transação</h1>
+			</Modal>
 		</div>
 	);
 };
--- src/presentation/components/shared/Header/index.tsx
+++ src/presentation/components/shared/Header/index.tsx
@@ -2,13 +2,19 @@ import { FC } from 'react';
 import logo from '../../../assets/logo.svg';
 import { HeaderStyles } from '../../../styles/components/shared/Header';
 
-export const Header: FC = () => {
+interface IHeaderProps {
+	onNewTransactionButtonClick(): void;
+}
+
+export const Header: FC<IHeaderProps> = ({ onNewTransactionButtonClick }) => {
+
 	return <HeaderStyles.Container>
 		<HeaderStyles.Content>
 			<img src={logo} alt="Jm-Money" />
-			<button>
+			<button onClick={onNewTransactionButtonClick}>
 				Nova transação
 			</button>
 		</HeaderStyles.Content>
+
 	</HeaderStyles.Container>;
 };
